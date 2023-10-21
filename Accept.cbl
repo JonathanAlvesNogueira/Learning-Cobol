341ba76b7da8cc70db1ca84c1c5d575f045bfd03
Jonathan <joth10@live.com>
2023-10-21 01:30:02 -0300

    Capturando itens da tela

---
+++ Accept.cbl
@@ -0,0 +1,25 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+         77 WS-ACEITA PIC X(10) VALUE SPACE.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+
+
+            DISPLAY "Hello world"
+            ACCEPT WS-ACEITA
+            DISPLAY WS-ACEITA
+            STOP RUN.
+
+
+
+
+       END PROGRAM YOUR-PROGRAM-NAME.
