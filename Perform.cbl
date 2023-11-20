789428b456bff5c79c73ba1953f64f53e2c3c82e
Jonathan <joth10@live.com>
2023-11-19 23:18:10 -0300

    Usando perform

---
+++ Perform.cbl
@@ -0,0 +1,32 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+
+
+           PERFORM S1.
+
+           S1 SECTION.
+           P1-INICIO.
+               DISPLAY "HELLO 1".
+           P1-FIM.
+
+           P2-INICIO.
+               DISPLAY "fIM".
+           P2-FIM.
+
+           P3-INICIO.
+               STOP RUN.
+           P3-FIM.
+
+
+       END PROGRAM YOUR-PROGRAM-NAME.
